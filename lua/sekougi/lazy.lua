fdbc1411c9a0364dd547e53d9c0e1c071e33d351
Egor Grigoryev <tomskiy_a@list.ru>
2025-10-09 21:29:21 +0300

    lsp

--- lua/sekougi/lazy.lua
+++ lua/sekougi/lazy.lua
@@ -11,4 +11,17 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("sekougi.plugins")
+require("lazy").setup(
+  {
+    { import = "sekougi.plugins" },
+  },
+  {
+    checker = {
+      enabled = true,
+      notify = false,
+    },
+    change_detection = {
+      notify = false,
+    },
+  }
+)
--- lua/sekougi/plugins/cmp.lua
+++ lua/sekougi/plugins/cmp.lua
@@ -1,52 +1,50 @@
 return {
     "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
     dependencies = {
-        "hrsh7th/cmp-buffer", -- source for text in buffer
-        "hrsh7th/cmp-path", -- source for file system paths
-        {
-            "L3MON4D3/LuaSnip",
-            version = "v2.*",
-            -- install jsregexp (optional!).
-            build = "make install_jsregexp",
-        },
-        "rafamadriz/friendly-snippets",
-        "onsails/lspkind.nvim", -- vs-code like pictograms
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path", 
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-cmdline", 
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
     },
     config = function()
         local cmp = require("cmp")
-        local lspkind = require("lspkind")
-        local luasnip = require("luasnip")
 
-        require("luasnip.loaders.from_vscode").lazy_load()
-
+        -- Основная настройка (для обычного редактирования)
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    luasnip.lsp_expand(args.body)
+                    require("luasnip").lsp_expand(args.body)
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.close(),
-                ["<CR>"] = cmp.mapping.confirm({
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
-                }),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
+            }, {
                 { name = "buffer" },
                 { name = "path" },
-            }),
-        })
+            })
+      })
 
-        vim.cmd([[
-      set completeopt=menuone,noinsert,noselect
-      highlight! default link CmpItemKind CmpItemMenuDefault
-    ]])
-    end,
+      -- Дополнение в командной строке
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+            { name = 'cmdline' }
+        })
+      })
+
+      -- Дополнение при поиске (/ и ?)
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+            { name = 'buffer' }
+        }
+      })
+  end,
 }
