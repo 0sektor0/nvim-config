855aa56c19797f475f20bd80cea813a32a87b932
Egor Grigoryev <tomskiy_a@list.ru>
2025-11-09 02:45:07 +0300

    lsp

--- lua/sekougi/plugins/cmp.lua
+++ lua/sekougi/plugins/cmp.lua
@@ -6,7 +6,6 @@ return {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-cmdline", 
         "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
     },
     config = function()
         local cmp = require("cmp")
