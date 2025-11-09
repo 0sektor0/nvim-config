c8deb76af594ce8c4ac1324403520d64c8642451
0sektor0 <egrigorew0@yandex.ru>
2025-11-10 02:43:29 +0300

    vague theme added

--- lua/sekougi/plugins/colorscheme.lua
+++ lua/sekougi/plugins/colorscheme.lua
@@ -1,7 +1,12 @@
 return {
-    "savq/melange-nvim",
-    config = function(_, opts)
-   	vim.opt.termguicolors = true
-  	vim.cmd.colorscheme 'melange' 
-    end,
+  "vague-theme/vague.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other plugins
+  config = function()
+    -- NOTE: you do not need to call setup if you don't want to.
+    require("vague").setup({
+      -- optional configuration here
+    })
+    vim.cmd("colorscheme vague")
+  end
 }
