992126a624abf474c1ffe2b93e3abe63aa999328
0sektor0 <egrigorew0@yandex.ru>
2025-11-16 03:44:07 +0300

    neotree commands

--- lua/sekougi/plugins/neotree.lua
+++ lua/sekougi/plugins/neotree.lua
@@ -5,6 +5,10 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+    config = function()
+        vim.keymap.set("n", "<leader>tb", "<cmd>Neotree show focus buffers right<cr>",{ desc = "Neotree toggle show buffers right" })
+        vim.keymap.set("n", "<leader>tg", "<cmd>Neotree float git_status<cr>",{ desc = "Neotree float git status" })
+        vim.keymap.set("n", "<leader>tt", "<cmd>Neotree show focus filesystem left<cr>",{ desc = "Neotree toggle" })
+    end
 }
