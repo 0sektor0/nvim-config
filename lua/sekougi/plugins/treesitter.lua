d453ce1664275883c3b05d7d3a0625344799e27e
ogas <sekougi@yandex.ru>
2026-03-01 18:58:28 +0300

    tree-sitter and telescope update

--- lua/sekougi/plugins/treesitter.lua
+++ lua/sekougi/plugins/treesitter.lua
@@ -7,32 +7,6 @@ return {
         "windwp/nvim-ts-autotag",
     },
     config = function()
-        require 'nvim-treesitter.configs'.setup {
-            -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-            ensure_installed = {
-                "lua",
-                "vim",
-                "vimdoc",
-                "markdown",
-                "markdown_inline",
-                "query",
-                "c",
-                "rust",
-                "c_sharp",
-                "toml"
-            },
-            sync_install = false,
-            auto_install = true,
-            ident = { enable = true },
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            rainbow = {
-                enable = true,
-                extende_mode = true,
-                max_file_lines = nil,
-            }
-        }
+        require('nvim-treesitter').install({ 'lua', 'vim', 'rust', 'c', 'toml', 'vimdoc', 'c_sharp' }):wait(300000)
     end,
 }
