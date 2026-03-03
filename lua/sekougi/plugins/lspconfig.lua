6d3de3e495d0790e52975a72f2355239df30a3c3
0sektor0 <egrigorew0@yandex.ru>
2026-03-04 00:19:53 +0300

    extra lsp removed

--- lua/sekougi/plugins/lspconfig.lua
+++ lua/sekougi/plugins/lspconfig.lua
@@ -15,26 +15,10 @@ function config_rust()
     vim.lsp.enable("rust_analyzer")
 end
 
-function config_roslyn_ls()
+function config_roslyn()
     vim.lsp.enable("roslyn_ls")
 end
 
-function config_roslyn()
-    vim.lsp.config("roslyn", {
-        on_attach = function()
-            print("This will run when the server attaches!")
-        end,
-        settings = {
-            ["csharp|inlay_hints"] = {
-                csharp_enable_inlay_hints_for_implicit_object_creation = true,
-                csharp_enable_inlay_hints_for_implicit_variable_types = true,
-            },
-            ["csharp|code_lens"] = {
-                dotnet_enable_references_code_lens = true,
-            },
-        },
-    })
-end
 
 function config_lua()
     vim.lsp.config(
@@ -104,8 +88,7 @@ return {
     },
     config = function()
         config_lua()
-        --config_roslyn()
-        config_roslyn_ls()
+        config_roslyn()
         config_rust()
         config_keymap()
     end
