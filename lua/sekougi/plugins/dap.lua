4fba280b0935aa8fb14ec3a214aabfdc2dbda866
Egor Grigoryev <tomskiy_a@list.ru>
2026-07-11 01:43:19 +0300

    add dotnet dap configurations

--- lua/sekougi/plugins/dap.lua
+++ lua/sekougi/plugins/dap.lua
@@ -99,6 +99,23 @@ local function config_cs()
     dap.configurations.cs = {
         {
             type = "coreclr",
+            name = "Launch .NET app",
+            request = "launch",
+            program = function()
+                return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+            stopAtEntry = false,
+            console = platform.console,
+        },
+        {
+            type = "coreclr",
+            name = "Attach to .NET process",
+            request = "attach",
+            processId = require("dap.utils").pick_process,
+        },
+        {
+            type = "coreclr",
             name = "Attach to Unity",
             request = "attach",
             processId = function()
